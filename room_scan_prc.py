03e0c31419bae689ca3eeaf57d4499e3a8d5e00b
Evangelos Pappas <vang@Evangeloss-MacBook-Pro-Aeolos.local>
2023-02-14 18:56:41 +0000

    better wall finder and top-bottom plane finding.

--- room_scan_prc.py
+++ room_scan_prc.py
@@ -17,6 +17,7 @@ def big_plane_decision(segment, plane_size_thres, plane_point_count_thres):
             big_dimension_count += 1
     if big_dimension_count >= 2:
         is_big_plane = True
+        print(bbox)
     if len(segment.points) < plane_point_count_thres:
         is_big_plane = False
 
@@ -60,6 +61,32 @@ def is_plane_contiguous(pcd):
         is_contig = False
     return is_contig
 
+def find_top_bottom_planes(horiz_plane_bboxes):
+    top_z, bottom_z = 1.0, 1.0
+    top_z_idx, bottom_z_idx = None, None
+    idx = 0
+    for bbox in horiz_plane_bboxes:
+        bbox_center = bbox.get_center()
+        bbox_z = bbox_center[2]
+        print(bbox_center)
+        if bbox_z >= top_z:
+            top_z = bbox_z
+            top_z_idx - idx
+        elif bbox_z <= top_z:
+            bottom_z = bbox_z
+            bottom_z_idx = idx
+        idx += 1
+    print("top_z, bottom_z: ", top_z, bottom_z)
+    return top_z_idx, bottom_z_idx
+
+def add_big_plane(plane, plane_pts, bbox, big_plane_count, bboxes, segment_planes, segments):
+            bboxes[big_plane_count] = bbox
+            segment_planes[big_plane_count] = plane
+            segments[big_plane_count]=plane_pts
+            segments[big_plane_count].paint_uniform_color(list(colors[:3]))
+            big_plane_count += 1
+            return big_plane_count
+
 #############
 args_len = len(sys.argv)
 
@@ -68,7 +95,7 @@ if args_len < 2:
     exit(0)
 
 input_file = sys.argv[1]
-dist_thres = 0.035 # meters
+dist_thres = 0.035 # meters. E.g. 0.035 is 35mm plane thicness threshold
 plane_size_thres = 2.5
 plane_point_count_thres = 300
 
@@ -90,33 +117,44 @@ if sub_rate > 1:
 #pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=16), fast_normal_computation=True)
 
 max_plane_idx = 15
-segment_models={}
+segment_planes={}
 segments={}
 bboxes={}
+horiz_plane_models=[]
+horiz_planes_pts=[]
+horiz_plane_bboxes=[]
 rest=pcd
 big_plane_count = 0
 for i in range(max_plane_idx):
     colors = plt.get_cmap("tab20")(i)
-    model, inliers = rest.segment_plane(
-    distance_threshold=dist_thres,ransac_n=3,num_iterations=1000)
-    segment=rest.select_by_index(inliers)
+    plane, inliers = rest.segment_plane(distance_threshold=dist_thres,ransac_n=3,num_iterations=1000)
+    plane_pts=rest.select_by_index(inliers)
 
     #check the physical size of the plane and choose only big ones.
-    is_big_plane, bbox, dims = big_plane_decision(segment, plane_size_thres, plane_point_count_thres)
-    is_horiz = is_plane_horizontal(model)
-    is_vert = is_plane_vertical(model)
-    is_contiguous = is_plane_contiguous(segment)
+    is_big_plane, bbox, dims = big_plane_decision(plane_pts, plane_size_thres, plane_point_count_thres)
+    is_horiz = is_plane_horizontal(plane)
+    is_vert = is_plane_vertical(plane)
+    is_contiguous = is_plane_contiguous(plane_pts)
     if is_contiguous and (is_horiz or is_vert):
     #if is_big_plane and (is_horiz or is_vert):
-        bboxes[big_plane_count] = bbox
-        segment_models[big_plane_count] = model
-        segments[big_plane_count]=segment
-        segments[big_plane_count].paint_uniform_color(list(colors[:3]))
-        big_plane_count += 1
+        if is_horiz: # keep track of the hor planes so later you only add the highest and lower
+            horiz_planes_pts.append(plane_pts)
+            horiz_plane_models.append(plane)
+            horiz_plane_bboxes.append(bbox)
+        elif is_vert:
+            big_plane_count = add_big_plane(plane, plane_pts, bbox, big_plane_count, bboxes, segment_planes, segments)
 
     rest = rest.select_by_index(inliers, invert=True)
     print("pass",i,"/",max_plane_idx,"done.")
     print("max - min bounds: ", dims, ", is big plane: ", is_big_plane)
+
+# find the top and the lower horizontal planes to eliminate the other inbetween
+# TODO: does not seem to work correctly... fix!
+top_plane_idx, bottom_plane_idx = find_top_bottom_planes(horiz_plane_bboxes)
+if top_plane_idx is not None:
+    big_plane_count = add_big_plane(horiz_plane_models[top_plane_idx], horiz_planes_pts[top_plane_idx], horiz_plane_bboxes[top_plane_idx], big_plane_count, bboxes, segment_planes, segments)
+if bottom_plane_idx is not None:
+    big_plane_count = add_big_plane(horiz_plane_models[bottom_plane_idx], horiz_planes_pts[bottom_plane_idx], horiz_plane_bboxes[bottom_plane_idx], big_plane_count, bboxes, segment_planes, segments)
 
 #o3d.visualization.draw_geometries([bboxes[i] for i in range(5)])
 
